719fbbe8de11369ebe17cd34f6b2413b5bbb4387
hayate nakayama <sippu242@gmail.com>
2017-08-10 01:16:50 +0900

    first
    first test

---
+++ plot.py
@@ -0,0 +1,12 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+x = np.arange(0, 6, 0.1)
+y = np.sin(x)
+z = np.cos(x)
+plt.plot(x, y)
+plt.plot(x, z)
+plt.show()
+print("やば，完璧やん")
+# eeeeeeeee
